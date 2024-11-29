4fc573ece445201b803004154e0ec0fcecaa95b5
bugg-m <mkchauhan0543@gmail.com>
2024-11-29 18:35:15 +0530

    environment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@features': '/src/features',
+      '@router': '/src/router',
+      '@static': '/src/static',
+      '@/types': '/src/types',
+      '@utils': '/src/utils',
+    },
+  },
+});
